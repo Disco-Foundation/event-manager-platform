6c564a64975ba8ce0f1f84204eafc2659261be71
Dayana Rodrigues <dayana2301@gmail.com>
2022-03-26 16:13:29 +0000

    fix: remove "readSyncFile"

--- libs/event-manager-certifiers/src/lib/get-certifier.ts
+++ libs/event-manager-certifiers/src/lib/get-certifier.ts
@@ -37,21 +37,24 @@ export const getCertifier = (type: Certifier) => {
 
   switch (type) {
     case Certifier.airdroper:
-      keyPairData = readJsonData(
-        CERTIFIERS_DATA_PATH + '/airdroper.keypair.json'
-      );
+      keyPairData = readJsonData(CERTIFIERS_DATA_PATH + '/a.json');
       break;
     case Certifier.productPayer:
-      keyPairData = readJsonData(
-        CERTIFIERS_DATA_PATH + '/productPayer.keypair.json'
-      );
+      keyPairData = {
+        s: [
+          60, 102, 88, 60, 29, 185, 28, 117, 255, 27, 18, 14, 64, 145, 15, 41,
+          19, 145, 79, 242, 64, 31, 3, 88, 227, 253, 39, 59, 98, 3, 80, 7, 220,
+          189, 7, 78, 28, 156, 115, 140, 23, 66, 0, 36, 128, 159, 16, 129, 178,
+          22, 78, 24, 171, 103, 4, 125, 110, 11, 89, 202, 129, 32, 148, 22,
+        ],
+      };
       break;
     case Certifier.testerUser:
-      keyPairData = readJsonData(CERTIFIERS_DATA_PATH + '/tester.keypair.json');
+      keyPairData = readJsonData(CERTIFIERS_DATA_PATH + '/t.json');
       break;
   }
 
   return Keypair.fromSecretKey(
-    new Uint8Array(keyPairData['secret'] as unknown as ArrayBufferLike)
+    new Uint8Array(keyPairData['s'] as unknown as ArrayBufferLike)
   );
 };
